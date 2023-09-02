80efe643013c7f456ee2bda89bb13de53dce3f4a
Andrei Belyi <andreybelyyw@gmail.com>
2023-09-02 14:12:26 -0600

    changes to counter placement

--- website/assets/js/index.js
+++ website/assets/js/index.js
@@ -1,8 +1,19 @@
 const counter = document.querySelector(".counter-number");
+
 async function updateCounter() {
-    let response = await fetch("https://hbo24wrldtx7bcunvjwrerwliu0nhvxf.lambda-url.us-east-1.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = ` This page has ${data} Views!`;
+    try {
+        let response = await fetch("https://hbo24wrldtx7bcunvjwrerwliu0nhvxf.lambda-url.us-east-1.on.aws/");
+
+        if (!response.ok) {
+            throw new Error(`Network response was not ok (${response.status} - ${response.statusText})`);
+        }
+
+        let data = await response.json();
+        counter.innerHTML = `This page has ${data} Views!`;
+    } catch (error) {
+        console.error("Error:", error);
+        // You can add code here to handle the error, such as displaying a message to the user.
+    }
 }
 
 updateCounter();
